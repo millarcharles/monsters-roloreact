931faeb986ec0d4d0758a68ccd218b4dcd37180c
millarcharles <ch.cmillar@me.comgit config --global user.email ch.cmillar@me.comgit config --global user.email ch.cmillar@me.com>
2019-07-18 11:36:46 -0400

    Updates

---
+++ precache-manifest.2a49f64f15583947ffea737f3af614ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73b041b043248fca50451dd30fb0791",
+    "url": "/monster-roloreact/index.html"
+  },
+  {
+    "revision": "4c5eebc32918a2e874cb",
+    "url": "/monster-roloreact/static/css/main.c8ea35c6.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/monster-roloreact/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "4c5eebc32918a2e874cb",
+    "url": "/monster-roloreact/static/js/main.e3620978.chunk.js"
+  },
+  {
+    "revision": "ade684b8d2f6b78cd0e8",
+    "url": "/monster-roloreact/static/js/runtime~main.a2e967b8.js"
+  }
+]);
